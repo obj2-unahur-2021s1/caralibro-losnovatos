9fe83614f57ed2c08e7fbb2a1df347825eb1960a
leozeballez <zeballezleonel@hotmail.com>
2021-04-27 21:22:04 -0300

    acomodados los tests

--- src/test/kotlin/ar/edu/unahur/obj2/caralibro/UsuarioTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/caralibro/UsuarioTest.kt
@@ -15,48 +15,6 @@ class UsuarioTest : DescribeSpec({
     val juan = Usuario()
     val sofia=Usuario()
 
-    describe("Una publicación") {
-      describe("de tipo foto") {
-        it("ocupa ancho * alto * compresion bytes") {
-          fotoEnCuzco.espacioQueOcupa().shouldBe(550503)
-        }
-      }
-      describe("de tipo foto con otro factor de compresion"){
-        it("ocupa ancho*alto*nuevaCompresion"){
-          FactorDeCompresion.cambiarFactorDeCompresion(0.8)
-          fotoEnCuzco.espacioQueOcupa().shouldBe(629146)
-        }
-      }
-
-      describe("de tipo texto") {
-        it("ocupa tantos bytes como su longitud") {
-          saludoCumpleanios.espacioQueOcupa().shouldBe(45)
-        }
-      }
-
-      describe("de tipo video") {
-        describe("calidad SD") {
-          it("ocupa bytes igual a la duración en segundos") {
-            videoFiesta.espacioQueOcupa().shouldBe(30)
-          }
-        }
-
-        describe("calidad HD720p") {
-          it("ocupa bytes igual al triple de duración en segundos") {
-            videoFiesta.cambiarACalidad(Calidad.HD720p)
-            videoFiesta.espacioQueOcupa().shouldBe(90)
-          }
-        }
-
-        describe("calidad HD1080p") {
-          it("ocupa bytes igual al doble de duración en segundos que tendría en HD720p") {
-            videoFiesta.cambiarACalidad(Calidad.HD1080p)
-            videoFiesta.espacioQueOcupa().shouldBe(180)
-          }
-        }
-      }
-    }
-
     describe("Un usuario") {
       it("puede calcular el espacio que ocupan sus publicaciones") {
         val juana = Usuario()
